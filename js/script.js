20603dcd9e672fec6acbca2b4e27565b83f16e39
Mastermxx <mreijgwart@gmail.com>
2020-06-19 00:51:21 +0200

    Meets Expecatations done

--- js/script.js
+++ js/script.js
@@ -1,101 +1,81 @@
 /******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
+Treehouse FSJS Techdegree:
+project 2 - List Filter and Pagination
+Script by Mark Reijgwart
+I am aiming for a "Exceeds Expectations" grade.
+If I don't get this grade I would like to redo it.
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-const showStudentsPerPage = 10;
-const studentListItem = document.getElementsByClassName('student-item');
 
-
-console.log(studentListItem);
-console.log(`total students: ${studentListItem.length}`);
-console.log(`students per page: ${showStudentsPerPage}`);
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const pageHeader = document.getElementsByClassName('page-header');
 
 
 
+const newDiv = document.createElement('div');                           // Create a new div element
+const allStudents = document.querySelector('.student-list').querySelectorAll('li'); // Get all the students
+const nodes = Array.from( allStudents );                                // Put all the students in an array
+const studentsPerPage = 10;                                             // Declare the wanted amount of students per page
+const totalPages = Math.ceil( allStudents.length / studentsPerPage );   // Get the page amount needed by deviding total students by studentsPerPage
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
+const pages = [];
+// For every 10 students create a chunk and push it to the pages variable
+const chunk = ( array, start, length ) => array.slice( start, length ); // Get all students and slice it in chunks
+for ( let i = 0; i < allStudents.length; i += studentsPerPage ) {       // Loop through all the students till there are no chunks of 10 left
+    pages.push( chunk ( nodes, i, i + studentsPerPage ) );              // Push chunks of 10 students to the empty array "pages"
+}
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-var currentPageNumber = 0;
+// This function will show and hide students based on the page number
+const showPage = (pageNumber) => {
 
-function showPage() {
-    for (let i = 0; studentListItem.length < studentListItem.length; i++) {
-        console.log(i)
-        // if (studentListItem.length > 10) {
-        //     studentListItem.style.display = "none";
-        // }
+    nodes.forEach( element => element.style.display = 'none' );         // By default hide all the students
+    const studentIndex = pageNumber * studentsPerPage;                  // Get the current pages multiply by the students per page (10)
+    const nodesToShow = chunk( nodes, studentIndex, studentIndex + studentsPerPage ); // Get the correct chunk of students to show
+    nodesToShow.forEach(element => element.style.display = 'block');    // Show the correct students for the current page
+}
+
+/* Create a button for all pages needed based on the results.
+**this is required for the Exceeds Expectations grade**/
+const appendPageLinks = () => {
+
+    const pageDiv = document.getElementsByClassName('page');            // Get the page div element
+    pageDiv[0].appendChild(newDiv).classList.add('pagination')          // Append a new div to the page div and give it a class "pagination"
+    const paginationDiv = document.getElementsByClassName('pagination');// Get the newly created pagination div element
+
+    for ( let index = 0; index < pages.length; index ++ ) {             // Loop through all pages (chunks) and do the following:
+
+        const listItem = document.createElement("li");                  // Create a list element
+        paginationDiv[0].appendChild(listItem)                          // Append the li to the pagination div
+        listItem.innerHTML = `<a href="#">${index + 1}</a>`;            // Create an anchor in the li with the correct page number
+
+        // Create a click function on the pagenumber that will show the correct student per page
+        listItem.addEventListener('click', () => {
+            showPage( index );
+        })
     }
+
+    showPage(0);                                                        // By default show the first page
 }
 
-// showPage();
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-// Create a pagination div for the buttons in the page div
-
-
-// let div = document.createElement('div');
-// document.getElementsByClassName('page').appendChild(div);
+appendPageLinks();
 
 
 
+// HTML STRUCTURE
+// <div class="student-search">
+//   <input placeholder="Search for students...">
+//   <button>Search</button>
+// </div>
 
 
-// Calculate the amount of pages needed based on the set students per page
-var pageAmountNeeded = Math.ceil(studentListItem.length / showStudentsPerPage);
-console.log(`pages needed: ${pageAmountNeeded}`);
-
-let pageDiv = document.getElementsByClassName('page');
-
-let div = document.createElement('div');
-div.class = 'note';
+const toSearch = 'wadawdwd'; // input.value
+const allNames = nodes.map((node)=>node.querySelector('h3').innerHTML);
+const filteredNames = allNames.filter(name => name.includes(toSearch));
+console.log(filteredNames);
 
 
-document.body.appendChild(div);
+/* When a search yields 0 results, a message is displayed on the page, informing the user that no results have been found.
+**this is required for the Exceeds Expectations grade**/
 
-for( let i = 1; i - 1 < pageAmountNeeded; i++ ) {
-    let button = document.createElement("button");
-    button.class = 'page';
-    div.appendChild(button);
-    button.textContent = `page ${i}`;
-    console.log(button.textContent = `page ${i}`)
+if (filteredNames.length === 0) {
+    console.log('empty')
 }
-
-for( let i = 1; i - 1 < pageAmountNeeded; i++ ) {
-    let listItem = document.createElement("li");
-    listItem.class = 'page';
-    div.appendChild(listItem);
-    listItem.innerHTML = `<a href="#">${i}</a>`;
-}
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
